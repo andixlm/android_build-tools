bac4f66421a0e4e3a28c43820fb047156c7209f0
andixlm <andixev@gmail.com>
2014-11-09 00:30:35 +0500

    build.sh: Improve and add argument checking

--- build.sh
+++ build.sh
@@ -7,8 +7,16 @@ export USE_CCACHE=1
 PASS=0
 
 ### Handle first argument
+# no first argument = ask for clean and device
+if [ -z $1 ]; then
+	echo -n 'Make it clean? (y/N): '
+	read CLEAN
+	if [ "$CLEAN" = "y" ]; then
+		make clean
+	fi
+
 # --s2ve = build for s2ve without cleaning
-if [ "$1" = "--s2ve" ]; then
+elif [ "$1" = "--s2ve" ]; then
 	PASS=1
 	DEVICE=s2ve
 
@@ -17,44 +25,46 @@ elif [ "$1" = "--s2vep" ]; then
 	PASS=1
 	DEVICE=s2vep
 
-# no first argument = ask for clean and device as usual
-elif [ "$1" = "" ]; then
-	echo -n "Make it clean? (y/N): "
-	read CLEAN
-	if [ "$CLEAN" = "y" ]; then
-		make clean
-	fi
+else
+	echo 'Wrong first argument! Stopping.'
+	exit 1
 fi
 ###
 
 ### Handle second argument
 # --mb = make boot image without the whole ROM building
-if [ "$2" = "--mb" ]; then
+if [ '-n $1' -a "$2" = "--mb" ]; then
 	SA=b
 
 # --mr = make recovery image without the whole ROM building
-elif [ "$2" = "--mr" ]; then
+elif [ '-n $1' -a "$2" = "--mr" ]; then
 	SA=r
+
+elif [ "$2" != "--mb" -o "$2" != "--mr" ]; then
+	echo 'Wrong second argument! Stopping.'
+	exit 1
 fi
 ###
 
-while [ $PASS -eq "0" ]; do
-	echo -n "Device? ('s2ve' or 's2vep'): "
+while [ $PASS -eq 0 ]; do
+	echo -n 'Device? (s2ve or s2vep): '
 	read DEVICE
 	if [ "$DEVICE" = "s2ve" ] || [ "$DEVICE" = "s2vep" ]; then 
 		PASS=1
 	else
-		echo "Wrong input! Try again."
+		echo Wrong input! Try again.
 	fi
 done
 
-if [ "$SA" = "" ]; then
+if [ -z $SA ]; then
 	. build/envsetup.sh
-	brunch "$DEVICE"
+	brunch $DEVICE
+
 elif [ "$SA" = "b" ]; then
 	. build/envsetup.sh
 	breakfast $DEVICE
 	mka bootimage
+
 elif [ "$SA" = "r" ]; then
 	. build/envsetup.sh
 	breakfast $DEVICE
