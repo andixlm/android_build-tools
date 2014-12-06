f5289efe7e89328804bae7e332bb2aed3da6135c
andixlm <andixev@gmail.com>
2014-12-06 19:41:33 +0500

    Update patches

--- patch.sh
+++ patch.sh
@@ -4,15 +4,17 @@ cd ..
 
 COUNT=0
 CURRENT_DIR="$PWD"
+PD="build_tools/patches"
 PATCHES=(
-'frameworks/av					build_tools/patches/frameworks_av.patch'
-'frameworks/base				build_tools/patches/frameworks_base.patch'
-'frameworks/native				build_tools/patches/frameworks_native.patch'
-'frameworks/opt/net/wifi		build_tools/patches/frameworks_opt_net_wifi.patch'
-'frameworks/opt/telephony		build_tools/patches/frameworks_opt_telephony.patch'
-'hardware/broadcom/libbt		build_tools/patches/hardware_broadcom_libbt.patch'
-'packages/services/Telephony	build_tools/patches/packages_services_Telephony.patch'
-'system/core					build_tools/patches/system_core.patch'
+'frameworks/av					frameworks_av.patch'
+'frameworks/base				frameworks_base.patch'
+'frameworks/native				frameworks_native.patch'
+'frameworks/opt/net/wifi		frameworks_opt_net_wifi.patch'
+'frameworks/opt/telephony		frameworks_opt_telephony.patch'
+'hardware/broadcom/libbt		hardware_broadcom_libbt.patch'
+'packages/services/Telephony	packages_services_Telephony.patch'
+'system/core					system_core.patch'
+'vendor/cm						vendor_cm.patch'
 )
 
 while [ "x${PATCHES[COUNT]}" != "x" ]
@@ -23,7 +25,7 @@ do
 
 	cd "$FOLDER"
 	echo Patching "$FOLDER" ...
-	git apply "$CURRENT_DIR/$PATCH"
+	git apply "$CURRENT_DIR/$PD/$PATCH"
 	cd "$CURRENT_DIR"
 
 	COUNT=$(($COUNT + 1))
